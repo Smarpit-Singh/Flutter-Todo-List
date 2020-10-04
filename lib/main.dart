df7b814e4847ba956e745fc5af2f5438c37dc198
Smarpit <smarpitsingh560@gmail.com>
2020-10-05 00:51:53 +0530

    Everything tied up

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_note_taking_app/screens/note_list.dart';
 
 void main() {
   runApp(MaterialApp(
     title: 'Note App',
+    home: NoteList(),
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
       brightness: Brightness.dark,
--- lib/models/note.dart
+++ lib/models/note.dart
@@ -25,4 +25,41 @@ class Note {
       this._title = value;
     }
   }
+
+  set description(String newDescription) {
+    if (newDescription.length <= 255) {
+      this._description = newDescription;
+    }
+  }
+
+  set priority(int newPriority) {
+    if (newPriority >= 1 && newPriority <= 2) {
+      this._priority = newPriority;
+    }
+  }
+
+  set date(String newDate) {
+    this._date = newDate;
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    if (id != null) {
+      map['id'] = _id;
+    }
+    map['title'] = _title;
+    map['description'] = _description;
+    map['priority'] = _priority;
+    map['date'] = _date;
+
+    return map;
+  }
+
+  Note.fromMapObject(Map<String, dynamic> map) {
+    this._id = map['id'];
+    this._title = map['title'];
+    this._description = map['description'];
+    this._priority = map['priority'];
+    this._date = map['date'];
+  }
 }
