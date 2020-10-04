304b04df39abdb06ae37f0a78e6fda62e98e4498
Smarpit <smarpitsingh560@gmail.com>
2020-10-04 22:03:10 +0530

    Initial commit

---
+++ android/app/src/main/java/flutter/buddy/com/flutter_note_taking_app/MainActivity.java
@@ -0,0 +1,6 @@
+package flutter.buddy.com.flutter_note_taking_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Note App',
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: Colors.blue,
+      accentColor: Colors.red,
+    ),
+  ));
+}
+
+
